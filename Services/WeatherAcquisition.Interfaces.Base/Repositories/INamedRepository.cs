987311380df293f71a737ccea25fd148b256abbd
Infarh <shmachilin@yandex.ru>
2021-06-13 17:14:27 +0300

    Интерфейс репозитория для именованных сущностей

---
+++ Services/WeatherAcquisition.Interfaces.Base/Repositories/INamedRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WeatherAcquisition.Interfaces.Base.Entities;
+
+namespace WeatherAcquisition.Interfaces.Base.Repositories
+{
+    public interface INamedRepository<T> : IRepository<T> where T : INamedEntity
+    {
+        Task<bool> ExistName(string Name, CancellationToken Cancel = default);
+
+        Task<T> GetByName(string Name, CancellationToken Cancel = default);
+
+        Task<T> DeleteByName(string Name, CancellationToken Cancel = default);
+    }
+}
