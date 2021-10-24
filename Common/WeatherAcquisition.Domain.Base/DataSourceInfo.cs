b4a2ed8a95b887f769a5b0e8cd50c235dab844c3
Infarh <shmachilin@yandex.ru>
2021-10-24 17:12:34 +0300

    Модели данных бизнес-логики

---
+++ Common/WeatherAcquisition.Domain.Base/DataSourceInfo.cs
@@ -0,0 +1,11 @@
+﻿using WeatherAcquisition.Interfaces.Base.Entities;
+
+namespace WeatherAcquisition.Domain.Base
+{
+    public class DataSourceInfo : INamedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
