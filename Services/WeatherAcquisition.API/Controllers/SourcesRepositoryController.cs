c031e0333952b9d2f7386fbd68498dcc7e2121ab
Infarh <shmachilin@yandex.ru>
2021-10-24 17:38:35 +0300

    Создан контроллер репозитория источников данных

---
+++ Services/WeatherAcquisition.API/Controllers/SourcesRepositoryController.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WeatherAcquisition.API.Controllers.Base;
+using WeatherAcquisition.DAL.Entities;
+using WeatherAcquisition.Domain.Base;
+using WeatherAcquisition.Interfaces.Base.Repositories;
+
+namespace WeatherAcquisition.API.Controllers
+{
+    public class SourcesRepositoryController : MappedEntityController<DataSourceInfo, DataSource>
+    {
+        public SourcesRepositoryController(IRepository<DataSource> Repository, IMapper Mapper) 
+            : base(Repository, Mapper)
+        {
+        }
+    }
+}
